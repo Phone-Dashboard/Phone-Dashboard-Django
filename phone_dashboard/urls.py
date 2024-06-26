ba58f16ebc0317f45bab30eaad34bf627935f602
Chris Karr <github@audacious-software.com>
2024-06-26 08:14:52 -0500

    Update urls.py

--- phone_dashboard/urls.py
+++ phone_dashboard/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import re_path, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^data/', include('passive_data_kit.urls')),
-    url(r'^monitor/', include('nagios_monitor.urls')),
-    url(r'^study/', include('study_support.urls')),
-    url(r'^', include('website.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^data/', include('passive_data_kit.urls')),
+    re_path(r'^monitor/', include('nagios_monitor.urls')),
+    re_path(r'^study/', include('study_support.urls')),
+    re_path(r'^', include('website.urls')),
 ]
