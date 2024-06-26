4c839b4d073cfdf4a86823c65912f4d68e8260be
Chris Karr <github@audacious-software.com>
2024-06-26 08:15:16 -0500

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,11 @@
 # pylint: disable=line-too-long
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import website_home, website_privacy, website_enroll
 
 urlpatterns = [
-    url(r'^$', website_home, name='website_home'),
-    url(r'^privacy$', website_privacy, name='website_privacy'),
-    url(r'^enroll$', website_enroll, name='website_enroll'),
+    re_path(r'^$', website_home, name='website_home'),
+    re_path(r'^privacy$', website_privacy, name='website_privacy'),
+    re_path(r'^enroll$', website_enroll, name='website_enroll'),
 ]
